3f94e417cb120afddb2d49135fa5fcfbbb5e8ce0
Gio J <147982243+gjagiashvili@users.noreply.github.com>
2024-04-27 23:09:26 +0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Products from "./components/Products";
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+      <Products />
+      <div className="view-all-products">
+        <button className="view-all-button">View all products</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
